07b844e8fe4834d6333f18038aee6fe39329af84
hongming.low <hongming.low@atoz-software.tech>
2025-09-17 23:59:05 +0800

    fix: update the default primary button

--- src/button/primary-button.svelte
+++ src/button/primary-button.svelte
@@ -1,5 +1,4 @@
-<script>
-  const { label = 'Button', ...props } = $props();
-</script>
-
-<button {...props}>{label}</button>
+<button
+  class="rounded-md bg-blue-600 px-4 py-2 text-white shadow-md shadow-blue-300 hover:cursor-pointer hover:bg-blue-500"
+  >Button</button
+>
